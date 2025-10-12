b0710d638e008e83dd895e4518e300917f9c0ef2
Michael McKIE <michael_mckie@hotmail.com>
2025-10-12 19:15:36 +0800

    Add project files.

---
+++ BowlingCoacher/BowlingCoacher/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BowlingCoacher
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ BowlingCoacher/BowlingCoacher/Services/FormFactor.cs
@@ -0,0 +1,17 @@
+using BowlingCoacher.Shared.Services;
+
+namespace BowlingCoacher.Services
+{
+    public class FormFactor : IFormFactor
+    {
+        public string GetFormFactor()
+        {
+            return DeviceInfo.Idiom.ToString();
+        }
+
+        public string GetPlatform()
+        {
+            return DeviceInfo.Platform.ToString() + " - " + DeviceInfo.VersionString;
+        }
+    }
+}
